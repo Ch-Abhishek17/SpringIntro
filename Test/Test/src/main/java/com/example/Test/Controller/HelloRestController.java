3f7acc7c3efd6d1b5a606c6fe7188d2c9bde482d
Ch-Abhishek17 <abhimanyu17abd@gmail.com>
2025-03-01 22:00:02 +0530

    Added to SpringIntro UC4

--- Test/Test/src/main/java/com/example/Test/Controller/HelloRestController.java
+++ Test/Test/src/main/java/com/example/Test/Controller/HelloRestController.java
@@ -8,9 +8,28 @@ import com.example.Test.model.User;
 @RequestMapping("/hello")
 public class HelloRestController {
 
+
+    // curl localhost:8080/hello -w "\n"
+    //uc1
     @GetMapping(value = {"", "/", "/home"})
     public String sayHello() {
         return "Hello From BridgeLabz! ! !";
 
     }
+
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name){
+        return "Hello " + name+" from Bridgelabz";
+    }
+
+    @GetMapping("/params/{name}")
+    public String sayHelloPathVariable(@PathVariable String name){
+        return "Hello " + name+" from Bridgelabz";
+    }
+
+    @PostMapping("/post")
+    public String sayHelloPost(@RequestBody User user){
+        return "Hello " + user.getFirstName()+" "+user.getLastName()+" from Bridgelabz";
+    }
+
 }
